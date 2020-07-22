6c8578f57c1f45207fe953febdf867c23b2c65d4
Sazzad Hossain <sharifh530@gmail.com>
2020-07-22 22:14:36 +0600

    using firebase library , included ability to store authenticated users to firebase database

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 import SignIn from "../../components/signin/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 import "./sign-in-and-sign-up.scss";
 
-const SignInAndSignUpPage = () => <SignIn className="sign-in-and-sign-up" />;
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SignInAndSignUpPage;
